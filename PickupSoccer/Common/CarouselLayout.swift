5be41042705f6b313c8e02f3a03573416b1be0b4
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-05-07 06:24:55 -0700

    contains files to ignore

---
+++ PickupSoccer/Common/CarouselLayout.swift
@@ -0,0 +1,9 @@
+//
+//  CarouselLayout.swift
+//  PickupSoccer
+//
+//  Created by Heriberto Rodriguez on 4/27/21.
+//  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
+//
+
+import Foundation
---
+++ PickupSoccer/Modules/Games/View/GameCVCell.swift
@@ -0,0 +1,9 @@
+//
+//  GameCVCell.swift
+//  PickupSoccer
+//
+//  Created by Heriberto Rodriguez on 5/3/21.
+//  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
+//
+
+import Foundation
