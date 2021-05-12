63922ebbd16a59fe5fe3eee259ca80f8c0448993
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-05-12 07:00:47 -0700

    make coordinate hashable

--- PickupSoccer/Extensions/CLLCoordinate2D+Ext.swift
+++ PickupSoccer/Extensions/CLLCoordinate2D+Ext.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
 //
 
-import Foundation
+import CoreLocation
+
+extension CLLocationCoordinate2D : Equatable {
+    public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+        return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+    }
+}
+
+extension CLLocationCoordinate2D : Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(latitude)
+        hasher.combine(longitude)
+    }
+}
