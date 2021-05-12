0ea99c8405d9b84215c7093a3de4d01ef369088a
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-05-12 07:00:17 -0700

    make coordinate hashable

---
+++ PickupSoccer/Extensions/CLLCoordinate2D+Ext.swift
@@ -0,0 +1,9 @@
+//
+//  CLLCoordinate2D+Ext.swift
+//  PickupSoccer
+//
+//  Created by Heriberto Rodriguez on 5/12/21.
+//  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
+//
+
+import Foundation
