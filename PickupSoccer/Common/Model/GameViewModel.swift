ce25130f7bcc596baa6fba02c958aaace6b3191a
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-05-19 07:12:38 -0700

    added new file

---
+++ PickupSoccer/Common/Model/GameViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  GameViewModel.swift
+//  PickupSoccer
+//
+//  Created by Heriberto Rodriguez on 5/19/21.
+//  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
+//
+
+import Foundation
