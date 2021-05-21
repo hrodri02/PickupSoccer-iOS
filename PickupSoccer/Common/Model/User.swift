d647d6d21c6b70ed79a68888cbcb275b0082ab2c
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-05-21 05:57:28 -0700

    added model to represent a user

--- PickupSoccer/Common/Model/User.swift
+++ PickupSoccer/Common/Model/User.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+struct User
+{
+    let firstName: String
+    let lastName: String
+    let position: Position
+}
+
+enum Position {
+    case goalKeeper
+    case centerBack
+    case sweeper
+    case fullBack
+    case wingBack
+    case centerMidfield
+    case defensiveMidfield
+    case attackingMidfield
+    case wideMidfield
+    case centerForward
+    case secondStriker
+    case winger
+}
