c6aa330697975a8749798b8b0f09ec65b6f1792e
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-05-24 07:29:39 -0700

    GameMo adopts and conforms to Game protocol

--- PickupSoccer/Extensions/GameMO+Ext.swift
+++ PickupSoccer/Extensions/GameMO+Ext.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
 //
 
-import Foundation
+import CoreLocation
+
+extension GameMO: Game
+{
+    var coordinate: CLLocationCoordinate2D {
+        get {
+            if let location = self.location {
+                return CLLocationCoordinate2D(latitude: location.latitude,
+                                              longitude: location.longitude)
+            }
+            else {
+                assertionFailure("location is nil")
+                return CLLocationCoordinate2D()
+            }
+        }
+        set {
+            location?.latitude = newValue.latitude
+            location?.longitude = newValue.longitude
+        }
+    }
+    
+    var timeInterval: DateInterval {
+        get {
+            if let dateInterval = self.dateInterval,
+                let start = dateInterval.start,
+                let end = dateInterval.end
+            {
+                return DateInterval(start: start, end: end)
+            }
+            else {
+                assertionFailure("dateInterval is nil")
+                return DateInterval()
+            }
+        }
+        set {
+            dateInterval?.start = newValue.start
+            dateInterval?.end = newValue.end
+        }
+    }
+}
