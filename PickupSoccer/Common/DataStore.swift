b3164221c5be64152cd2a4cc27759e8d0f292587
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-05-24 07:33:02 -0700

    moved DataStore to its own file

--- PickupSoccer/Common/DataStore.swift
+++ PickupSoccer/Common/DataStore.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
 //
 
-import Foundation
+import CoreLocation
+
+protocol DataStore {
+    func fetchGames(center: CLLocationCoordinate2D,
+                    latitudeDelta: CLLocationDegrees,
+                    longitudeDelta: CLLocationDegrees,
+                    completion: @escaping (Result<[Game], Error>) -> Void)
+    func saveGame(_ address: String,
+                  _ location: CLLocationCoordinate2D,
+                  _ dateInterval: DateInterval,
+                  completion: @escaping (Error?) -> Void)
+}
