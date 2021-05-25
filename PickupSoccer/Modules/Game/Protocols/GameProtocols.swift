f68c8829508f162869ae6b2bf0df16226d2a0024
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-05-25 07:57:28 -0700

    added main files for games module

---
+++ PickupSoccer/Modules/Game/Protocols/GameProtocols.swift
@@ -0,0 +1,9 @@
+//
+//  GameProtocols.swift
+//  PickupSoccer
+//
+//  Created by Heriberto Rodriguez on 5/25/21.
+//  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
+//
+
+import Foundation
---
+++ PickupSoccer/Modules/Game/Router/GameRouter.swift
@@ -0,0 +1,9 @@
+//
+//  GameRouter.swift
+//  PickupSoccer
+//
+//  Created by Heriberto Rodriguez on 5/25/21.
+//  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
+//
+
+import Foundation
