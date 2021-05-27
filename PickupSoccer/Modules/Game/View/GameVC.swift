c02121319cec625e2ae4f1fe8a184bf7b221d778
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-05-27 05:21:38 -0700

    display home team with soccer field in background

--- PickupSoccer/Modules/Game/View/GameVC.swift
+++ PickupSoccer/Modules/Game/View/GameVC.swift
@@ -11,14 +11,67 @@ import UIKit
 class GameVC: UIViewController {
     weak var presenter: GameViewToGamePresenter?
     
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .brown
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(HomeTeamCVCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionView.isPagingEnabled = true
+        collectionView.backgroundView = self.imageView
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
     deinit {
         print("GameVC deinit")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(imageView)
+        view.addSubview(collectionView)
+        setImageViewConstraints()
+        setCollectionViewConstraints()
+        
+        if let imageURL = Bundle.main.url(forResource: "pexels-photo-2291006", withExtension: "jpeg") {
+            do {
+                let imageData = try Data(contentsOf: imageURL)
+                imageView.image = UIImage(data: imageData)
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
         
         view.backgroundColor = UIColor.systemBlue.withAlphaComponent(0.7)
+    }
+    
+    private func setImageViewConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    
+    private func setCollectionViewConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
 }
 
@@ -29,5 +82,22 @@ extension GameVC: GamePresenterToGameView {
     
     func displayErrorMessage(_ errorMessage: String) {
         presentErrorMessage(errorMessage)
+    }
+}
+
+extension GameVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! HomeTeamCVCell
+        return cell
+    }
+}
+
+extension GameVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: collectionView.bounds.height)
     }
 }
