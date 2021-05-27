1d566b0f4e33c92785bc536f444157d205e368f8
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-05-27 05:45:39 -0700

    new file added

---
+++ PickupSoccer/Modules/Game/View/AwayTeamCVCell.swift
@@ -0,0 +1,9 @@
+//
+//  AwayTeamCVCell.swift
+//  PickupSoccer
+//
+//  Created by Heriberto Rodriguez on 5/27/21.
+//  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
+//
+
+import Foundation
