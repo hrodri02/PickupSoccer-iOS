8539d55530ff1c152100df38c5deb09962d04c6e
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-05-27 05:33:54 -0700

    added function to get soccer player image and to add subviews

--- PickupSoccer/Modules/Game/View/HomeTeamCVCell.swift
+++ PickupSoccer/Modules/Game/View/HomeTeamCVCell.swift
@@ -63,33 +63,36 @@ class HomeTeamCVCell: UICollectionViewCell
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        if let imageURL = Bundle.main.url(forResource: "pexels-photo-5246967", withExtension: "jpeg") {
-            do {
-                let imageData = try Data(contentsOf: imageURL)
-                goalieImageView.image = UIImage(data: imageData)
-                goalieImageView.layer.cornerRadius = imageViewHeight / 2.0
-                goalieImageView.clipsToBounds = true
-                
-                addImageViews(to: defenseStackView, count: 4, data: imageData)
-                addImageViews(to: midFieldStackView, count: 4, data: imageData)
-                addImageViews(to: forwardStackView, count: 2, data: imageData)
-            }
-            catch {
-                print(error.localizedDescription)
-            }
+        if let imageData = getSoccerPlayerImage() {
+            goalieImageView.image = UIImage(data: imageData)
+            goalieImageView.layer.cornerRadius = imageViewHeight / 2.0
+            goalieImageView.clipsToBounds = true
+            
+            addImageViews(to: defenseStackView, count: 4, data: imageData)
+            addImageViews(to: midFieldStackView, count: 4, data: imageData)
+            addImageViews(to: forwardStackView, count: 2, data: imageData)
         }
         
-        addSubview(verticalStackView)
-        verticalStackView.addArrangedSubview(goalieImageView)
-        verticalStackView.addArrangedSubview(defenseStackView)
-        verticalStackView.addArrangedSubview(midFieldStackView)
-        verticalStackView.addArrangedSubview(forwardStackView)
+        addSubviews()
         setVerticalStackViewConstraints()
         setGoalieImageViewConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func getSoccerPlayerImage() -> Data? {
+        if let imageURL = Bundle.main.url(forResource: "pexels-photo-5246967", withExtension: "jpeg") {
+            do {
+                let imageData = try Data(contentsOf: imageURL)
+                return imageData
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
     }
     
     private func addImageViews(to stackView: UIStackView, count: Int, data: Data) {
@@ -102,6 +105,14 @@ class HomeTeamCVCell: UICollectionViewCell
             imageView.widthAnchor.constraint(equalToConstant: imageViewHeight).isActive = true
             stackView.addArrangedSubview(imageView)
         }
+    }
+    
+    private func addSubviews() {
+        addSubview(verticalStackView)
+        verticalStackView.addArrangedSubview(goalieImageView)
+        verticalStackView.addArrangedSubview(defenseStackView)
+        verticalStackView.addArrangedSubview(midFieldStackView)
+        verticalStackView.addArrangedSubview(forwardStackView)
     }
     
     private func setVerticalStackViewConstraints() {
