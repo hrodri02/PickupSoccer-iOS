cad84679d1882e275151dd64e65a0a968c9d5a95
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-06-01 08:37:15 -0700

    added some properties to User and made is Hashble to test adding/removing a user from a game. Some of the properties will be changed to allow a user to be a part of many games

--- PickupSoccer/Common/Model/User.swift
+++ PickupSoccer/Common/Model/User.swift
@@ -10,22 +10,34 @@ import Foundation
 
 struct User
 {
+    let uid: Int
     let firstName: String
     let lastName: String
-    let position: Position
+    var isWithHomeTeam: Bool
+    var position: Position
+}
+
+extension User: Hashable {
+    static func == (lhs: User, rhs: User) -> Bool {
+        return lhs.uid == rhs.uid
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(uid)
+    }
 }
 
 enum Position {
     case goalKeeper
-    case centerBack
-    case sweeper
-    case fullBack
-    case wingBack
-    case centerMidfield
-    case defensiveMidfield
-    case attackingMidfield
-    case wideMidfield
-    case centerForward
-    case secondStriker
-    case winger
+    case leftFullBack
+    case leftCenterBack
+    case rightCenterBack
+    case rightFullBack
+    case leftSideMidfielder
+    case leftCenterMidfield
+    case rightCenterMidField
+    case rightSideMidfielder
+    case leftCenterForward
+    case rightCenterForward
+    case none
 }
