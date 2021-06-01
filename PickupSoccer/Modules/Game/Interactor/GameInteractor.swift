d743313a2ae3b83d89714617ed77b3514566df9d
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-06-01 14:51:28 -0700

    added logic for changing the team of a user and for removing them from a game

--- PickupSoccer/Modules/Game/Interactor/GameInteractor.swift
+++ PickupSoccer/Modules/Game/Interactor/GameInteractor.swift
@@ -10,7 +10,12 @@ import Foundation
 
 class GameInteractor {
     weak var presenter: GameInteractorToGamePresenter?
-    
+    var positionsOccupiedInHomeTeam = Set<Position>()
+    var positionsOccupiedInAwayTeam = Set<Position>()
+    var homeTeam = Set<User>()
+    var awayTeam = Set<User>()
+    // TODO: - this is for testing only
+    var user: User!
     deinit {
         print("GameInteractor deinit")
     }
@@ -18,9 +23,109 @@ class GameInteractor {
 
 extension GameInteractor: GamePresenterToGameInteractor {
     func fetchPlayersForGame() {
-        let players = [User(firstName: "Heriberto", lastName: "Rodriguez", isWithHomeTeam: true, position: .leftCenterMidfield),
-                       User(firstName: "Brayan", lastName: "Rodriguez", isWithHomeTeam: false, position: .leftFullBack),
-                       User(firstName: "Reyna", lastName: "Ramirez", isWithHomeTeam: true, position: .leftFullBack)]
-        presenter?.onFetchPlayersSuccess(players)
+        let players = [User(uid: 1,
+                            firstName: "Heriberto",
+                            lastName: "Rodriguez",
+                            isWithHomeTeam: true,
+                            position: .leftCenterMidfield),
+                       User(uid: 2,
+                            firstName: "Brayan",
+                            lastName: "Rodriguez",
+                            isWithHomeTeam: false,
+                            position: .leftFullBack),
+                       User(uid: 3,
+                            firstName: "Reyna",
+                            lastName: "Ramirez",
+                            isWithHomeTeam: true,
+                            position: .leftFullBack)]
+        user = players[0]
+        
+        for player in players {
+            if player.isWithHomeTeam {
+                positionsOccupiedInHomeTeam.insert(player.position)
+                homeTeam.insert(player)
+            }
+            else {
+                positionsOccupiedInAwayTeam.insert(player.position)
+                awayTeam.insert(player)
+            }
+        }
+        
+        presenter?.onFetchPlayersSuccess(homeTeam, awayTeam)
+    }
+    
+    func updateUserPosition(_ position: Position, isHomeTeam: Bool) {
+        if isPositionFree(position, isPositionForHomeTeam: isHomeTeam) {
+            removeUserFromCurrentTeam()
+            updateUser(newPosition: position, isNewPositionInHomeTeam: isHomeTeam)
+            addUpdatedUserToTeam(isNewTeamHomeTeam: isHomeTeam)
+            markNewPositionAsOccupied(position, isNewPositionInHomeTeam: isHomeTeam)
+            presenter?.onUpdatedTeams(homeTeam, awayTeam)
+        }
+    }
+    
+    private func isPositionFree(_ position: Position, isPositionForHomeTeam: Bool) -> Bool {
+        if isPositionForHomeTeam {
+            return !positionsOccupiedInHomeTeam.contains(position)
+        }
+        
+        return !positionsOccupiedInAwayTeam.contains(position)
+    }
+    
+    private func removeUserFromCurrentTeam() {
+        if user.isWithHomeTeam {
+            positionsOccupiedInHomeTeam.remove(user.position)
+            homeTeam.remove(user)
+        }
+        else {
+            positionsOccupiedInAwayTeam.remove(user.position)
+            awayTeam.remove(user)
+        }
+    }
+    
+    private func updateUser(newPosition: Position, isNewPositionInHomeTeam: Bool) {
+        user.isWithHomeTeam = isNewPositionInHomeTeam
+        user.position = newPosition
+    }
+    
+    private func addUpdatedUserToTeam(isNewTeamHomeTeam: Bool) {
+        if isNewTeamHomeTeam {
+            homeTeam.insert(user)
+        }
+        else {
+            awayTeam.insert(user)
+        }
+    }
+    
+    private func markNewPositionAsOccupied(_ position: Position, isNewPositionInHomeTeam: Bool) {
+        if isNewPositionInHomeTeam {
+            positionsOccupiedInHomeTeam.insert(position)
+        }
+        else {
+            positionsOccupiedInAwayTeam.insert(position)
+        }
+    }
+    
+    func checkIfUserIsPartOfGame() {
+        if user.position == .none {
+            presenter?.onUserIsNotPartOfGame()
+        }
+        else {
+            presenter?.onUserIsPartOfGame()
+        }
+    }
+    
+    func removeUserFromGame() {
+        if user.isWithHomeTeam {
+            homeTeam.remove(user)
+            positionsOccupiedInHomeTeam.remove(user.position)
+        }
+        else {
+            awayTeam.remove(user)
+            positionsOccupiedInAwayTeam.remove(user.position)
+        }
+        
+        user.position = .none
+        presenter?.onUpdatedTeams(homeTeam, awayTeam)
     }
 }
