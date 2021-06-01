835efaaa83d79d717a63822ab73865d26db1c549
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-06-01 08:13:53 -0700

    new cell that HomeTeamCVCell and AwayTeamCVCell inherit from

---
+++ PickupSoccer/Modules/Game/View/TeamCVCell.swift
@@ -0,0 +1,9 @@
+//
+//  TeamCVCell.swift
+//  PickupSoccer
+//
+//  Created by Heriberto Rodriguez on 6/1/21.
+//  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
+//
+
+import Foundation
