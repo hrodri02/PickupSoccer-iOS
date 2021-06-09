2acd9b34862b9a609f27fccbc67a6cf4d224fb83
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-06-09 06:38:25 -0700

    added new files

---
+++ PickupSoccer/Common/UserManager.swift
@@ -0,0 +1,9 @@
+//
+//  UserManager.swift
+//  PickupSoccer
+//
+//  Created by Heriberto Rodriguez on 6/4/21.
+//  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
+//
+
+import Foundation
