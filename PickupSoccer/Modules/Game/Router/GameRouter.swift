e4def72c463135585aeb4a0292148ebe4ad03781
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-06-22 08:47:31 -0700

    added methods to present and dismiss PlayerInfoVC

--- PickupSoccer/Modules/Game/Router/GameRouter.swift
+++ PickupSoccer/Modules/Game/Router/GameRouter.swift
@@ -13,6 +13,23 @@ class GameRouter: GamePresenterToGameRouter {
         print("GameRouter deinit")
     }
     
+    func presentPlayerInfoVC(_ viewController: GameVC, presenter: GamePresenter) {
+        let playerInfoVC = PlayerInfoVC()
+        playerInfoVC.presenter = presenter
+        playerInfoVC.delegate = viewController
+        playerInfoVC.modalPresentationStyle = .popover
+        playerInfoVC.modalTransitionStyle = .crossDissolve
+        
+        presenter.playerInfoView = playerInfoVC
+        
+        let navController = UINavigationController(rootViewController: playerInfoVC)
+        viewController.present(navController, animated: true, completion: nil)
+    }
+    
+    func dismissPlayerInfoVC(_ navigationController: UINavigationController) {
+        navigationController.dismiss(animated: true, completion: nil)
+    }
+    
     func dismiss(_ navigationController: UINavigationController) {
         navigationController.popToRootViewController(animated: true)
     }
