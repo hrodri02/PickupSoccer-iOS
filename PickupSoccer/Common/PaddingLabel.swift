7509b478da0b63a36d70e369f6ac59b47473e6a7
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-06-24 06:47:24 -0700

    added new file

---
+++ PickupSoccer/Common/PaddingLabel.swift
@@ -0,0 +1,9 @@
+//
+//  PaddingLabel.swift
+//  PickupSoccer
+//
+//  Created by Heriberto Rodriguez on 6/24/21.
+//  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
+//
+
+import Foundation
