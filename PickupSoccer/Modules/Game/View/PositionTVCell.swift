89b7c558dcff0b246f72258a2d6ee2e1851d62f8
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-06-24 06:51:49 -0700

    table view cell containing position name and checkmark if position is selected

--- PickupSoccer/Modules/Game/View/PositionTVCell.swift
+++ PickupSoccer/Modules/Game/View/PositionTVCell.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2021 Heriberto Rodriguez. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PositionTVCell: UITableViewCell
+{
+    let checkmarkImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "checkmark")
+        imageView.tintColor = .systemTeal
+        imageView.isHidden = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = UIColor.init(white: 0.15, alpha: 1.0)
+        textLabel?.textColor = UIColor.init(white: 0.9, alpha: 1.0)
+        separatorInset = .zero
+        
+        addSubview(checkmarkImageView)
+        NSLayoutConstraint.activate([
+            checkmarkImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            checkmarkImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10.0),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func configure(with positionName: String, isSelected: Bool) {
+        checkmarkImageView.isHidden = true
+        
+        textLabel?.text = positionName
+        checkmarkImageView.isHidden = !isSelected
+    }
+}
