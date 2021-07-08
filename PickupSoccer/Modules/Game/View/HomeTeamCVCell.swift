e250f0f13ea3e3297c21f18ac41ae4e52d7b81f8
Heriberto Rodriguez <bray@Brayans-MacBook-Pro.local>
2021-07-08 07:43:06 -0700

    Added a color parameter to setBorderColorOfImageViews and two different methods for adding subviews so that this cell class can be used for the home team and away team.

--- PickupSoccer/Modules/Game/View/HomeTeamCVCell.swift
+++ PickupSoccer/Modules/Game/View/HomeTeamCVCell.swift
@@ -8,14 +8,13 @@
 
 import UIKit
 
-class HomeTeamCVCell: UICollectionViewCell
+class TeamCVCell: UICollectionViewCell
 {
     static let verticalPaddingBeforeFirstImageView: CGFloat = SCREEN_HEIGHT * 0.10
     static let deltaXBetweenImageViews: CGFloat = 10.0
     var imageViews = [UIImageView]()
-    var soccerPlayerImageData: Data?
     var imageViewHeight: CGFloat {
-        return (bounds.width - 5.0 * HomeTeamCVCell.deltaXBetweenImageViews) / 4.0
+        return (bounds.width - 5.0 * TeamCVCell.deltaXBetweenImageViews) / 4.0
     }
     
     lazy var verticalStackView: UIStackView = {
@@ -23,7 +22,7 @@ class HomeTeamCVCell: UICollectionViewCell
         stackView.axis = .vertical
         stackView.distribution = .fillEqually
         stackView.alignment = .center
-        stackView.spacing = (bounds.height - imageViewHeight * 4.0 - 2 * HomeTeamCVCell.verticalPaddingBeforeFirstImageView) / 3.0
+        stackView.spacing = (bounds.height - imageViewHeight * 4.0 - 2 * TeamCVCell.verticalPaddingBeforeFirstImageView) / 3.0
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
@@ -41,7 +40,7 @@ class HomeTeamCVCell: UICollectionViewCell
     let defenseStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
-        stackView.spacing = HomeTeamCVCell.deltaXBetweenImageViews
+        stackView.spacing = TeamCVCell.deltaXBetweenImageViews
         stackView.distribution = .fillEqually
         stackView.alignment = .fill
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -51,7 +50,7 @@ class HomeTeamCVCell: UICollectionViewCell
     let midFieldStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
-        stackView.spacing = HomeTeamCVCell.deltaXBetweenImageViews
+        stackView.spacing = TeamCVCell.deltaXBetweenImageViews
         stackView.distribution = .fillEqually
         stackView.alignment = .fill
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -61,7 +60,7 @@ class HomeTeamCVCell: UICollectionViewCell
     let forwardStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
-        stackView.spacing = HomeTeamCVCell.deltaXBetweenImageViews
+        stackView.spacing = TeamCVCell.deltaXBetweenImageViews
         stackView.distribution = .fillEqually
         stackView.alignment = .fill
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -70,54 +69,16 @@ class HomeTeamCVCell: UICollectionViewCell
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = UIColor.systemRed.withAlphaComponent(0.5)
-        
         goalieImageView.layer.cornerRadius = imageViewHeight / 2.0
         goalieImageView.clipsToBounds = true
         
         addImageViews(to: defenseStackView, count: 4, row: 1)
         addImageViews(to: midFieldStackView, count: 4, row: 2)
         addImageViews(to: forwardStackView, count: 2, row: 3)
-        
-        setBorderColorOfImageViews()
-        
-        addSubviews()
-        setVerticalStackViewConstraints()
-        setGoalieImageViewConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    public func configure(with players: [String : Position]) {
-        for imageView in imageViews {
-            imageView.image = nil
-            imageView.alpha = 0.0
-        }
-        
-        let imageData = getSoccerPlayerImage()
-        for (_, position) in players {
-            if let data = imageData {
-                let index = Int(position.rawValue)
-                imageViews[index].alpha = 1.0
-                imageViews[index].image = UIImage(data: data)
-            }
-        }
-    }
-    
-    private func getSoccerPlayerImage() -> Data? {
-        if let imageURL = Bundle.main.url(forResource: "pexels-photo-5246967", withExtension: "jpeg") {
-            do {
-                let imageData = try Data(contentsOf: imageURL)
-                soccerPlayerImageData = imageData
-                return imageData
-            }
-            catch {
-                print(error.localizedDescription)
-            }
-        }
-        return nil
     }
     
     private func addImageViews(to stackView: UIStackView, count: Int, row: Int) {
@@ -141,13 +102,13 @@ class HomeTeamCVCell: UICollectionViewCell
         }
     }
     
-    func setBorderColorOfImageViews() {
+    func setBorderColorOfImageViews(color: UIColor) {
         for imageView in imageViews {
-            imageView.layer.borderColor = UIColor.systemRed.withAlphaComponent(0.9).cgColor
+            imageView.layer.borderColor = color.cgColor
         }
     }
     
-    func addSubviews() {
+    func addSubviewsForHomeTeam() {
         addSubview(verticalStackView)
         verticalStackView.addArrangedSubview(goalieImageView)
         verticalStackView.addArrangedSubview(defenseStackView)
@@ -155,12 +116,25 @@ class HomeTeamCVCell: UICollectionViewCell
         verticalStackView.addArrangedSubview(forwardStackView)
     }
     
+    func addSubviewsForAwayTeam() {
+        addSubview(verticalStackView)
+        verticalStackView.addArrangedSubview(forwardStackView)
+        verticalStackView.addArrangedSubview(midFieldStackView)
+        verticalStackView.addArrangedSubview(defenseStackView)
+        verticalStackView.addArrangedSubview(goalieImageView)
+    }
+    
+    func setConstraints() {
+        setVerticalStackViewConstraints()
+        setGoalieImageViewConstraints()
+    }
+    
     private func setVerticalStackViewConstraints() {
         NSLayoutConstraint.activate([
-            verticalStackView.topAnchor.constraint(equalTo: topAnchor, constant: HomeTeamCVCell.verticalPaddingBeforeFirstImageView),
-            verticalStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: HomeTeamCVCell.deltaXBetweenImageViews),
-            verticalStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -HomeTeamCVCell.deltaXBetweenImageViews),
-            verticalStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -HomeTeamCVCell.verticalPaddingBeforeFirstImageView)
+            verticalStackView.topAnchor.constraint(equalTo: topAnchor, constant: TeamCVCell.verticalPaddingBeforeFirstImageView),
+            verticalStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: TeamCVCell.deltaXBetweenImageViews),
+            verticalStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -TeamCVCell.deltaXBetweenImageViews),
+            verticalStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -TeamCVCell.verticalPaddingBeforeFirstImageView)
         ])
     }
     
